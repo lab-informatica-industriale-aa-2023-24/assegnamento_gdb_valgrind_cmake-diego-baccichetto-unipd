9943bb57023f216bbce2e341729e67bbb36fb8ee
Diego Baccichetto <diego.baccichetto@studenti.unipd.it>
2024-05-09 15:20:47 +0200

    Nel programma valgrind1.c aggiornata la dimensione della memoria da allocare

--- valgrind/valgrind1.c
+++ valgrind/valgrind1.c
@@ -2,11 +2,15 @@
 // In questo file ci sono due problemi
 
 #include <stdlib.h>
+#include <stdio.h>
 
 void f(void)
 {
-	int* x = malloc(10 * sizeof(int));
-	x[10] = 0;
+	int* x = malloc((10+1) * sizeof(int));
+	/*Se alloco 10 interi in memoria vuol dire che posso accedere solo 
+		ai valori con indice da 0 a 9, se accedo all'indice 10 accedo ad un area
+		non allocata, devo allocare una cella in più per accedere a x[10]*/
+	x[10] = 0;	
 }
 
 int main(void)
