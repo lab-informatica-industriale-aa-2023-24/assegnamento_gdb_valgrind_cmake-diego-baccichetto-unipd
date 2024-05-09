c83a4d3b5b7d5755b0bfddc5328ce1fa1d9b6e35
Diego Baccichetto <diego.baccichetto@studenti.unipd.it>
2024-05-09 16:56:05 +0200

    Modificato allocazione di memoria nel programma valgrind4.c

--- valgrind/valgrind4.c
+++ valgrind/valgrind4.c
@@ -4,7 +4,8 @@
 
 int main(void) {
 	// allocate lo spazio per una stringa nello heap
-	char* s = malloc(strlen("Hello World!"));
+	char* s = malloc(strlen("Hello World!")+1);	/*strcpy copia anche il terminatore di stringa
+							quindi devo allocare un unità di memoria in più*/
 	strcpy(s, "Hello World!");
 
 	printf("%s\n", s);
